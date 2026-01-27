2b55299af7bbc245fa9da444e6107c695c82b7ad
fmusenene <fmusenene@gmail.com>
2026-01-27 15:49:00 +0300

    Update footer copyright year and translations

--- include/footer.php
+++ include/footer.php
@@ -62,14 +62,7 @@ if (!defined('SITE_NAME')) {
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span><?php echo t('footer_copyright'); ?></span> <strong class="px-1 sitename"><?php echo e(SITE_NAME); ?></strong> <span><?php echo t('footer_rights'); ?></span></p>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
+      <p>© <span><?php echo t('footer_copyright'); ?></span> <?php echo convertNumbers(date('Y')); ?> <strong class="px-1 sitename"><?php echo e(SITE_NAME); ?></strong> <span><?php echo t('footer_rights'); ?></span></p>
     </div>
 
   </footer>
